10d19df6b6001655a19295ebc24e74db7b9e6799
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-03 13:09:44 +0300

    plantesFonts

--- MauiPlanets/MauiPlanets/MauiPlanets/MauiProgram.cs
+++ MauiPlanets/MauiPlanets/MauiPlanets/MauiProgram.cs
@@ -11,8 +11,10 @@ namespace MauiPlanets
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+                    fonts.AddFont("Montserrat-Semibold.ttf", "MediumFont");
+                    fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
+
                 });
 
 #if DEBUG
