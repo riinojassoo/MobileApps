d7b94172c441c2ce82ec5ff3881ae078f4d11ef0
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-09 13:24:11 +0300

    PlanetsStartPage

--- MauiPlanets/MauiPlanets/MauiPlanets/App.xaml.cs
+++ MauiPlanets/MauiPlanets/MauiPlanets/App.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace MauiPlanets
+﻿using MauiPlanets.Views;
+
+namespace MauiPlanets
 {
     public partial class App : Application
     {
+        const int WindowWidth = 540;
+        const int WindowHeight = 1200;
+        //teha iseseisvalt vaate automaatne avanemine
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new StartPage();
         }
     }
 }
