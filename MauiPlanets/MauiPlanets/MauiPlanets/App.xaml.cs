0cbe804291d821d3fce2f29f479e95633668904a
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-09 19:27:28 +0300

    PlanetsStartingWindowSize

--- MauiPlanets/MauiPlanets/MauiPlanets/App.xaml.cs
+++ MauiPlanets/MauiPlanets/MauiPlanets/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using MauiPlanets.Views;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+using MauiPlanets.Views;
 
 namespace MauiPlanets
 {
@@ -11,7 +17,20 @@ namespace MauiPlanets
         {
             InitializeComponent();
 
-            MainPage = new StartPage();
+
+#if WINDOWS
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) => 
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight)); 
+        });
+#endif
+			MainPage = new StartPage();
         }
     }
 }
